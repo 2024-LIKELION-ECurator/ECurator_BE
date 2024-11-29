79219a26e031c7ffc797e66461cec55724017a75
noeyadd <wkdekdus6778@naver.com>
2024-11-30 00:08:29 +0900

    fix: diary update

--- ecurator/diary/views.py
+++ ecurator/diary/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .models import Diary
 from .serializers import DiarySerializer
+from datetime import date
 
 # Create
 class DiaryCreateView(APIView):
@@ -14,7 +15,7 @@ class DiaryCreateView(APIView):
         if serializer.is_valid():
             serializer.save(author=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_200_OK)
 
 # Update
 class DiaryUpdateView(APIView):
@@ -22,15 +23,18 @@ class DiaryUpdateView(APIView):
 
     def put(self, request, pk):
         try:
-            diary = Diary.objects.get(pk=pk, author=request.user)
+            # 오늘 날짜 가져오기
+            today = date.today()
+            # 주어진 pk와 작성자, 오늘 날짜가 일치하는 일기 가져오기
+            diary = Diary.objects.get(pk=pk, author=request.user, created_at__date=today)
         except Diary.DoesNotExist:
-            return Response({"error": "Diary not found or not authorized"}, status=status.HTTP_200_OK)
+            return Response({"error": "Diary not found, not authorized, or not today's diary"}, status=status.HTTP_200_OK)
         
         serializer = DiarySerializer(diary, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_200_OK)
 
 # Detail
 class DiaryDetailView(APIView):
@@ -43,7 +47,7 @@ class DiaryDetailView(APIView):
             return Response({"error": "Diary not found or not authorized"}, status=status.HTTP_200_OK)
         
         serializer = DiarySerializer(diary)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 # Delete
 class DiaryDeleteView(APIView):
